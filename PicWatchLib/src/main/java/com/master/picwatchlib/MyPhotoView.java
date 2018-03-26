064ad798233b3ea15b2fc15e565b1a404681c19e
公司 <951827860@qq.com>
2018-03-26 15:51:20 +0800

    Initial commit

---
+++ PicWatchLib/src/main/java/com/master/picwatchlib/MyPhotoView.java
@@ -0,0 +1,96 @@
+package com.master.picwatchlib;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+
+import com.github.chrisbanes.photoview.OnScaleChangedListener;
+import com.github.chrisbanes.photoview.PhotoView;
+
+import java.io.Serializable;
+
+class MyPhotoView extends PhotoView implements OnScaleChangedListener{
+
+    private Paint paint=null;
+    private boolean finish=false;
+    private Float textLength=null;
+    private int dy=0;
+
+    private CloseListener closeListener;
+
+    public MyPhotoView(Context context) {
+        this(context,null);
+    }
+
+    public MyPhotoView(Context context, AttributeSet attr) {
+        this(context, attr,0);
+    }
+
+    public MyPhotoView(Context context, AttributeSet attr, int defStyle) {
+        super(context, attr, defStyle);
+        initxx();
+    }
+
+    private void initxx() {
+        setOnScaleChangeListener(this);
+        setMinimumScale(0.1f);
+        paint=new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(0xffffffff);
+        paint.setTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,16,getResources().getDisplayMetrics()));
+        textLength=paint.measureText("您可以单击/缩小返回");
+        Paint.FontMetrics fontMetrics = paint.getFontMetrics();
+        dy= (int) ((fontMetrics.ascent - fontMetrics.descent) / 2 - fontMetrics.ascent);
+    }
+
+    @Override
+    public void onScaleChange(float scaleFactor, float focusX, float focusY) {
+        if (getScale() < 0.15f) {
+            if (closeListener != null) {
+                if (!finish) {
+                    closeListener.close();
+                    finish = true;
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+//        paint.setColor(0x60666666);
+//        canvas.drawRect(getWidth() / 2 - textLength / 2, getHeight() - textLength / 10, getWidth() / 2 + textLength / 2,getHeight(), paint);
+//        paint.setColor(0xffffffff);
+//        canvas.drawText("您可以单击/缩小返回", getWidth() / 2 - textLength / 2, (getHeight() - textLength / 20 + dy), paint);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_POINTER_UP || event.getAction() == MotionEvent.ACTION_CANCEL) {
+            if (getScale() <= 0.6f) {
+                setScale(0.1f, true);
+                return true;
+            } else if (getScale() < 1f) {
+                setScale(1f, true);
+                return true;
+            }
+
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    public CloseListener getCloseListener() {
+        return closeListener;
+    }
+
+    public void setCloseListener(CloseListener closeListener) {
+        this.closeListener = closeListener;
+    }
+
+    public interface CloseListener {
+        void close();
+    }
+}
---
+++ PicWatchLib/src/main/java/com/master/picwatchlib/PicFragment.java
@@ -0,0 +1,165 @@
+package com.master.picwatchlib;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 不听话的好孩子 on 2018/3/26.
+ */
+
+public class PicFragment extends Fragment {
+    private ArrayList<String> urls;
+    private int index = 0;
+    private TextView indexview;
+    private ViewPager viewPager;
+
+    private MyPhotoView.CloseListener listener;
+
+    public static void Go(FragmentActivity activity, ArrayList<String> urls, int index, @NonNull View shared) {
+        activity.getSupportFragmentManager().beginTransaction()
+                .add(android.R.id.content, new PicFragment().setIndex(index)
+                        .setUrls(urls))
+                .addSharedElement(shared, "picxx")
+                .addToBackStack(null)
+                .commit();
+
+    }
+
+    public PicFragment setChildCloseListener(MyPhotoView.CloseListener listener) {
+        this.listener = listener;
+        return this;
+    }
+
+    public PicFragment setUrls(ArrayList<String> urls) {
+        this.urls = urls;
+        return this;
+    }
+
+    public PicFragment setIndex(int index) {
+        this.index = index;
+        return this;
+    }
+
+    public List<String> getUrls() {
+        return urls;
+    }
+
+    public String getSelectedUrl() {
+        return urls.get(getIndex());
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.pic_watch_layout, container, false);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (urls != null) {
+            outState.putSerializable("urls", urls);
+        }
+        outState.putInt("index", index);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (savedInstanceState != null) {
+            index = savedInstanceState.getInt("index");
+            urls = (ArrayList<String>) savedInstanceState.getSerializable("urls");
+        }
+        viewPager = view.findViewById(R.id.viewpager);
+        indexview = view.findViewById(R.id.index);
+        indexview.setText(String.format("%s/%s", index + 1, urls.size()));
+        viewPager.setAdapter(new FragmentPagerAdapter(getFragmentManager()) {
+            @Override
+            public android.support.v4.app.Fragment getItem(int position) {
+                EachPicFragment eachPicFragment = new EachPicFragment().setUrl(urls.get(position));
+                if (listener != null) {
+                    eachPicFragment.setlistener(listener);
+                }
+                return eachPicFragment;
+            }
+
+            @Override
+            public int getCount() {
+                return urls.size();
+            }
+        });
+        viewPager.setCurrentItem(index, false);
+        viewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                index = position;
+                indexview.setText(String.format("%s/%s", position + 1, urls.size()));
+                super.onPageSelected(position);
+            }
+        });
+    }
+
+    public static class EachPicFragment extends Fragment {
+        private String url;
+        private MyPhotoView.CloseListener listener;
+
+        public EachPicFragment setUrl(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public EachPicFragment setlistener(MyPhotoView.CloseListener listener) {
+            this.listener = listener;
+            return this;
+        }
+
+        @Override
+        public void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            outState.putString("url", url);
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.photoview, container, false);
+        }
+
+        @Override
+        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            if (savedInstanceState != null) {
+                url = savedInstanceState.getString("url");
+            }
+            MyPhotoView photoView = view.findViewById(R.id.photoview);
+            if (listener != null) {
+                photoView.setCloseListener(listener);
+            } else {
+                photoView.setMinimumScale(0.65f);
+            }
+            Glide.with(photoView)
+                    .load(url)
+                    .into(photoView);
+        }
+
+    }
+}
